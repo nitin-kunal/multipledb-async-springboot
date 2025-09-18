8d2fe9f5c65dd3025ec05bfa66156972c124c69d
nitin-kunal <72296757+nitin-kunal@users.noreply.github.com>
2025-09-18 21:32:52 +0530

    Delete grok-multiple-dbs directory

--- grok-multiple-dbs/src/main/java/com/nitin/grok/dao/db2/StateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nitin.grok.dao.db2;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StateRepository extends JpaRepository<StateEntity, Long> {
-}
-
